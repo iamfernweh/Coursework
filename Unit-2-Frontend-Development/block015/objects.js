1dec3339ec97cda3540e197c6a844be3a3cb0fd6
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-01-29 12:18:39 -0500

    object learning

---
+++ Unit-2-Frontend-Development/block015/objects.js
@@ -0,0 +1,40 @@
+//Declare an object named dinner that uses the purchased 
+//food as the keys and the price of the food for the values.
+
+let dinner = {
+    hamburger: 12,
+    steak: 20,
+    soup: 8,
+    macAndCheese: 14,
+    soupAndSalad: 16,
+};
+
+//List all of the different foods that were eaten during the dinner.
+//List the prices of the meals eaten during the dinner.
+console.log(Object.keys(dinner));
+console.log(Object.values(dinner));
+
+// Using what we know about objects, 
+// add the values of the dinner object to calculate the 
+// total cost of the meal. 
+
+let prices = Object.values(dinner);
+let totalCost = 0;
+
+for (let i = 0; i < prices.length; i++){
+    totalCost += prices[i];
+}
+
+console.log(totalCost);
+
+//Using what we know about objects, 
+//add the values of the dinner object to calculate 
+//the total cost of the meal. 
+
+function calculateCost(me) {
+    let total = 0;
+    for (let food in meal){
+        total += meal[food];
+    }
+    return total;
+}
