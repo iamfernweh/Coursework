c62bc26348c53408299079ecfa8a2afe544aefcf
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-01-30 11:45:46 -0500

    fixing stuff

--- Unit-2-Frontend-Development/block016/forEach.js
+++ Unit-2-Frontend-Development/block016/forEach.js
@@ -1,14 +1,11 @@
-// Define a function that takes another function (callback) as an argument
-function greet(callback) {
-    // Call the callback function
-    callback();
-  }
-  
-  // Define a callback function
-  function sayHello() {
-    console.log("Hello!");
-  }
-  
-  // Call the greet function and pass the sayHello function as a callback
-  greet(sayHello);
-  
+const studentNames = [
+    'julie', 'elissa', 'juan', 'lucas', 'allison'
+];
+
+studentNames.forEach(s => console.log(`Welcome to class, ${s}!`))
+
+const clogEachName = (oneName) => {
+    console.log(oneName);
+}
+
+studentNames.forEach(clogEachName)
