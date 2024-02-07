14f7b73cc1dcb55ade658be2146afae6286ac7a5
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-07 18:01:03 -0500

    updates

--- Unit-2-Frontend-Development/block21/starter/index.js
+++ Unit-2-Frontend-Development/block21/starter/index.js
@@ -1,17 +1,22 @@
-const API_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/DEMO/recipes';
+const API_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/DEMO/recipes";
 
-const recipes = []
+const state = {
+  recipes: [],
+  // could add other pieces of state
+};
 
-const recipesList = document.querySelector('#recipes');
+const recipesList = document.querySelector("#recipes");
 
-const addRecipeForm = document.querySelector('#addRecipe');
-addRecipeForm.addEventListener('submit', addRecipe);
+const addRecipeForm = document.querySelector("#addRecipe");
+addRecipeForm.addEventListener("submit", createRecipe);
 
 /**
  * Sync state with the API and rerender
  */
 async function render() {
   // TODO
+  await getRecipes();
+  renderRecipes();
 }
 render();
 
@@ -19,7 +24,16 @@ render();
  * Update state with recipes from API
  */
 async function getRecipes() {
-  // TODO
+  try {
+    // fetch recipe data
+    const response = await fetch(API_URL);
+    // parse response - translate JSON into a JS object
+    const json = await response.json();
+    // update 'recipes' array
+    state.recipes = json.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 /**
@@ -27,7 +41,27 @@ async function getRecipes() {
  * @param {Event} event
  */
 async function createRecipe(event) {
-    // TODO
+  // TODO
+  event.preventDefault();
+
+  const name = addRecipeForm.title.value;
+  const imageUrl = addRecipeForm.imageUrl.value;
+  const description = addRecipeForm.description.value;
+
+  try {
+    // fetch recipe data
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, imageUrl, description }),
+    });
+    // parse response
+    const json = await response.json();
+    // rerender the page (which will update 'recipes' array)
+    render();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 /**
@@ -48,6 +82,18 @@ async function updateRecipe(id, name, imageUrl, description) {
  */
 async function deleteRecipe(id) {
   // TODO
+  try {
+    //  make a DELETE request
+    const response = await fetch(`${API_UEL}/${id}`, {
+      method: "DELETE",
+    });
+    // we can parse response (we didn't)
+    // we can do things with the response (we didn't)
+    // rerender everything
+    render();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 /**
@@ -55,4 +101,37 @@ async function deleteRecipe(id) {
  */
 function renderRecipes() {
   // TODO
+  if (state.recipes.length < 1) {
+    const newListItem = document.createElement("li");
+    newListItem.textContent = "No recipes found.";
+    recipesList.append(newListItem);
+  } else {
+    recipesList.replaceChildren(); // --> should empty the recipes list element
+    // forEach over each recipe object
+    state.recipes.forEach((recipeObj) => {
+      // create a <li> with a heading, image, paragraph</li>
+
+      const newListItem = document.createElement("li");
+      newListItem.classList.add("recipe");
+
+      const newHeading = document.createElement("h2");
+      newHeading.textContent = recipeObj.name;
+
+      const newImage = document.createElement("img");
+      newImage.src = recipeObj.imageUrl;
+
+      const newParagraph = document.createElement("p");
+      newParagraph.textContent = recipeObj.description;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", () => deleteRecipe(recipeObj.id));
+
+      newListItem.append(newHeading, newImage, newParagraph, deleteButton);
+      // append to `recipes` (parent element)
+
+      recipesList.append(newListItem);
+    });
+  }
+  // recipeList
 }
