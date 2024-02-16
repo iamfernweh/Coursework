964900eb1934ab34197407289ef9c76a84d21829
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-16 12:00:38 -0500

    block 25

---
+++ Unit-3-State-Management/block25/props-demo/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Block 25 Props Demo - Grocery List</h1>
+
+    </>
+  )
+}
+
+export default App
