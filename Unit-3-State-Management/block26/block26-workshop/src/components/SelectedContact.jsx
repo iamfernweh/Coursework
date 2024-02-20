7e90dc5f3ce0d48d302aaf390544a872d8c0d235
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-20 11:17:48 -0500

    updates

--- Unit-3-State-Management/block26/block26-workshop/src/components/SelectedContact.jsx
+++ Unit-3-State-Management/block26/block26-workshop/src/components/SelectedContact.jsx
@@ -1,15 +1,28 @@
-export default function SelectedContact ({selectedContactId}) {
-    
-    {
+import { useEffect, useState } from "react";
 
-    }
-    
-    
+export default function SelectedContact({ selectedContactId }) {
+    const [singleContact, setSingleContact] = useState(null);
+
+
+    useEffect(() => {
+        async function fetchContact() {
+            try{
+                const response = await fetch(`https://jsonplaceholder.typicode.com/users/${selectedContactId}`);
+                const result = await response.json();
+                console.log(result);
+            
+        } catch (error) {
+            console.log(error);
+        }}
+        fetchContact();
+    }, [selectedContactId]);
+
+
     return (
         <>
-        {selectedContactId === null 
-            ? <div>No contact selected</div> 
-            : <div>{selectedContactId}</div>}
+            {selectedContactId === null
+                ? <div>No contact selected</div>
+                : <div>{selectedContactId}</div>}
         </>
-    )
+    );
 }
