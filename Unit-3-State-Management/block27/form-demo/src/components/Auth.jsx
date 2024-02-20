1ec0acaadb8ea52e5553a8597e67706377d8edc5
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-20 14:41:42 -0500

    new auth

--- Unit-3-State-Management/block27/form-demo/src/components/Auth.jsx
+++ Unit-3-State-Management/block27/form-demo/src/components/Auth.jsx
@@ -1,31 +1,35 @@
 import { useState } from "react";
 
 function Auth({token}) {
-    const [success, setSuccess] = useState(false);
-    setSuccess(result.success)
- async function attemptAuth(){
-        try {
-            const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', 
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    "Authorization": `Bearer ${token}`
-                }
-            })
-            const result = await response.json()
-        } catch (err) {
-            console.error(err);
+  const [success, setSuccess] = useState(false)
+  console.log(token)
+
+  async function attemptAuth() {
+    try {
+      const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate',
+      {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      })
+      const result = await response.json()
+      setSuccess(result.success)
+      console.log(result)
+    } catch (err){
+      console.error(err)
     }
+  }
 
-
-    return(
-        <>
-            <button onClick={attemptAuth}>Authenticate</button>
-            {success ? <div>Successfully Auth</div>
-            : <div>Unsuccessful</div>}
-        </>
-    )
+  return (
+    <>
+    {/* button to authenticate a request with the token */}
+    <button onClick={attemptAuth}>Authenticate!</button>
+    {/* message: successful or not? */}
+    {success ? <div>Successfully Authenticated!</div> : <div>Unsuccessful :(</div>}
+    </>
+  )
 }
 
-export default Auth
+export default Auth;
