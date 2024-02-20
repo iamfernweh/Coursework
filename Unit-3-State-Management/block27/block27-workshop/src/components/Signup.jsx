7bbcda7411a0135610b706904ea8b0a98da5ab49
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-20 15:14:55 -0500

    add submits

--- Unit-3-State-Management/block27/block27-workshop/src/components/Signup.jsx
+++ Unit-3-State-Management/block27/block27-workshop/src/components/Signup.jsx
@@ -5,6 +5,20 @@ export default function SignUpForm() {
     const [password, setPassword] = useState('');
     const [error, setError] = useState('null');
 
+async function handleSubmit(event){
+    event.preventDefault();
+    console.log('Hello');
+}
 
-    return <h2>Sign Up</h2>;
+    return (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+          </label>
+          <label>
+            Password: <input value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+          <button>Submit</button>
+        </form>
+      )
 }
