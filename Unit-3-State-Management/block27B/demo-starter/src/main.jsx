438b098b2d9d96fda77c2392ddf6bc47a9d2b4ea
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-23 12:16:26 -0500

    updates

--- Unit-3-State-Management/block27B/demo-starter/src/main.jsx
+++ Unit-3-State-Management/block27B/demo-starter/src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// 1. Import <Provider>
-// 2. Import `store`
+import { Provider } from "react-redux";
+import store from './app/store.js';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // 3. Wrap <App> in <Provider>
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
