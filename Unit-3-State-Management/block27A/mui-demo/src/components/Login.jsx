1adb21f7b3f7e053964f7e4ee1702f2fb1b048e8
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-23 11:07:32 -0500

    msg

--- Unit-3-State-Management/block27A/mui-demo/src/components/Login.jsx
+++ Unit-3-State-Management/block27A/mui-demo/src/components/Login.jsx
@@ -6,13 +6,23 @@ export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (username === 'admin' && password === '1234') {
+            alert('You have logged in');
+        }
+        else {
+            alert('You shall not enter');
+        }
+    }
+
     return (
         <>
             <h2>Login</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <TextField id='username' label='Username' variant="outlined" required value={username} onChange={(e) => { setUsername(e.target.value); }} /><br></br><br></br>
                 <TextField id='password' label='Password' variant="outlined" type="password" required value={password} onChange={(e) => { setPassword(e.target.value); }} /><br></br><br></br>
-                <Button variant="outlined">Login</Button>
+                <Button variant="outlined" type='submit' >Login</Button>
             </form>
         </>
     );
